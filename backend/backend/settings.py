f83d88602ed059d24a74905a6d4642f3667785a0
LloydTao <LL23@live.co.uk>
2020-12-26 03:30:04 +0000

    Initialise minitip app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts.apps.AccountsConfig",
+    "minitip.apps.MinitipConfig",
 ]
 
 MIDDLEWARE = [
---
+++ backend/minitip/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MinitipConfig(AppConfig):
+    name = "minitip"
