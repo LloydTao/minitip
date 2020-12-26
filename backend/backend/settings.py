df7c004098f50990d70efd449192f858b4e677a5
LloydTao <LL23@live.co.uk>
2020-12-26 22:37:52 +0000

    Refactor tailwind into django app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "accounts.apps.AccountsConfig",
     "minitip.apps.MinitipConfig",
+    "tailwind.apps.TailwindConfig",
 ]
 
 MIDDLEWARE = [
